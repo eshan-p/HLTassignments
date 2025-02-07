96d313e1329b1c6b74b362141bdf74fb38f28917
Eshan Patel <133532859+eshan-p@users.noreply.github.com>
2025-02-07 15:10:04 -0600

    create python program

---
+++ A1_DATASET/A1.py
@@ -0,0 +1,27 @@
+# Names (NetID): 
+# Kenneth Anttila (kea200001)
+# Aditya Desai    (amd210008)
+# Tanmaye Goel    (txg220006)
+# Eshan Patel     (exp200016)
+
+# Section: CS 4395.001
+# Assignment 1
+
+"""
+1. Unsmoothed n-grams
+    - compute unigram & bigram probabilities from the training corpus
+    - can only use libraries for preprocessing (corpus already tokenized tho)
+"""
+
+"""
+2. Smoothing & unknown words
+    - 1) implement a method (or multiple methods) to handle unknown words
+    - 2) implement the two smoothing methods (ex. Laplace & Add-k smoothing w/ different k)
+"""
+
+"""
+3. Perplexity on Validation Set
+    - compute the perplexity of the development/validation set --> val.txt
+    - equation given in pdf
+    - if we use more than one type of smoothing and unknown word handling, compare perplexity results in report
+""" 
